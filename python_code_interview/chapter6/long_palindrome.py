78c556d22d0ef899c3ec8ceb77c7c0e78e7ef59e
sky81219 <=>
2023-12-13 15:59:43 +0900

    util: typing and 주석

--- python_code_interview/chapter6/long_palindrome.py
+++ python_code_interview/chapter6/long_palindrome.py
@@ -1,17 +1,24 @@
-def long_palindrome(s: str) -> str:
-    def expend(left: int, right: int) -> str:
+def longest_palidrome(s: str) -> str:
+    """
+    팰린드롬
+    """
+    # 팰린드롬 판별 및 투 포인터 확장
+    def expand(left: int, right: int) -> str:
         while left >= 0 and right < len(s) and s[left] == s[right]:
             left -= 1
             right += 1
-        return s[left + 1 : right]
-
+        return s[left + 1: right]
+    
+    # 해당 사항이 없다면 빠르게 리턴
     if len(s) < 2 or s == s[::-1]:
         return s
 
-    result: str = ""
+    result = ''
+
+    # 슬라이딩 윈도우 우측으로 이동
     for i in range(len(s) - 1):
-        result = max(result, expend(i, i + 1), expend(i, i + 2), key=len)
+        result = max(result, expand(i, i+1), expand(i, i+2), key=len)   
     return result
 
 
-print(long_palindrome("babadb"))
+print(longest_palidrome("cbbd"))
--- python_code_interview/chapter6/palindrome.py
+++ python_code_interview/chapter6/palindrome.py
@@ -1,42 +1,25 @@
+"""
+chapter 1.
+"""
 import re
 from collections import deque
 
+input_str = "A man, a plan, a canal: Panama"
 
-def palindrome_deque(s: str) -> bool:
-    # 자료형 데크로 선언
-    strs: deque = deque()
 
-    for char in s:
+def solution(strs: str) -> bool:
+    string_data = []
+    
+    for char in strs:
         if char.isalnum():
-            strs.append(char.lower())
-
-    while len(strs) > 1:
-        if strs.popleft() != strs.pop():
+            string_data.append(char.lower())
+    
+    
+    while len(string_data) > 1:
+        if string_data.pop(0) != string_data.pop():
             return False
-
+    
     return True
 
 
-def palidrome_list(s: str) -> bool:
-    strs = []
-    for char in s:
-        if char.isalnum():
-            strs.append(char.lower())
-
-    while len(strs) > 1:
-        if strs.pop(0) != strs.pop():
-            return False
-
-    return True
-
-
-def palidrome_slicing(s: str) -> bool:
-    s = s.lower()
-    # 정규 표현식 이용하기
-    s: str = re.sub(r"[^a-z0-9]", "", s)
-
-    return s == s[::-1]
-
-
-input_str: str = "A man, a plan, a canal: Panama"
-print(palidrome_slicing(input_str))
+print(solution(input_str))

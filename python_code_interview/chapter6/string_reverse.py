05a59dc044f89415aea44b633d21993735d94c70
sky81219 <=>
2024-02-07 20:29:29 +0900

    fact: 1차-완성

--- python_code_interview/chapter6/string_reverse.py
+++
@@ -1,9 +0,0 @@
-def reserve_string(s: list[str]) -> None:
-    left, right = 0, len(s) - 1
-    while left < right:
-        s[left], s[right] = s[right], s[left]
-        left += 1
-        right -= 1
-
-
-print(reserve_string(["h", "e", "l", "l", "o"]))
